af6db465aa69cf8658c66cdaffd0c04fc6c1c2cb
Imurlovco <162899396+imurlovco@users.noreply.github.com>
2025-11-29 14:06:42 +0000

    Initial structure

---
+++ app.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.requests import Request
+from fastapi.responses import HTMLResponse
+import requests
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
